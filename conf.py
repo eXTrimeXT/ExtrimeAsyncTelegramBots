b81ed5a51fc95518259f7855ad994a742a098aac
eXTrimeXT <57262756+eXTrimeXT@users.noreply.github.com>
2021-04-07 15:35:51 +0300

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,34 +1,28 @@
 
-
+# ДАННЫЕ, КОТОРЫЕ МЕНЯТЬ МОЖНО
 account_number = 1 # начала диапозона
 max_account_number = 8  #  максимальное кол-во аккаунтов
-max_cycle = 4  #  максимальное кол-во циклов для бота
-max_bad_status_code = 10  # максильное кол-во плохих HTTP статус кодов
-BOT_NAME = "lite" # имя бота(пользователя): litecoin, dogecoin
-BOT_NAME += "coin_click_bot"
-BOT_TITLE = "LTC"  # заголовок БОТА: DOGE, LTC
-BOT_TITLE += " Click Bot"
-
 BOT_QUEUE = 1  # *Сколько ботов будут работать одновременно*
-
-
+max_cycle = 4  #  максимальное кол-во циклов для бота
+max_bad_status_code = 10  # максильное кол-во плохих HTTP статус кодов | менять можно, но хз зачем
 LTC_ADDRESS = "MQLRETwKE3P2ThYKMnfysgZ9G3M6VZNpoD"  # Вам адресс LTC-криптокошелька
-MINIMAL_BALANCE = 0.00100010  # Комиссия = 0.00000008
 
+# НЕ МЕНЯЙ ЭТИ ДАННЫЕ, ИНАЧЕ ТЫ СЛОМАЕШЬ КОНФИГУРАЦИЮ
 # Use your own values from my.telegram.org
 """ CLIENT_CONF """                                 #configuration for telegram client 
-api_id = 50322                                      #api id  ### DONT CHANGE! ###
+api_id = 50322                                      #api id 
 api_hash = '9ff1a639196c0779c86dd661af8522ba'       #api hash
+MINIMAL_BALANCE = 0.00100010  # Комиссия = 0.00000008 | Эту переменную можно изменить, но только в том случае, если 
+BOT_NAME = "lite" # имя бота(пользователя): litecoin, dogecoin
+BOT_NAME += "coin_click_bot"
+BOT_TITLE = "LTC"  # заголовок БОТА: LTC, DOGE
+BOT_TITLE += " Click Bot"  
 
-# Почта для восстановления: 
-# hw060935@gmail.com
+
 # Рефералка для ботов: 
 # https://t.me/Litecoin_click_bot?start=kXh2R 
 
-# ["5", "+79606137893", "3055106", "10fb2dac961e4061dc2b8940362ae37b", "MQLRETwKE3P2ThYKMnfysgZ9G3M6VZNpoD", "extrimesystemadmin777666"],
-# Dont have a session
-
-# ["ID", "PHONE", API_ID", "API_HASH", "dogecoin", "litecoin", "password"]
+# ["ID", "PHONE", "password"]
 Accounts = (
     ["id", "номер телефона", "пароль от аккаунта"],
     ["1", "+79845879573509", "парольпароль"],
