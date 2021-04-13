b3cba8068e3f89bf9f886772d5c2285a1e53c41a
eXTrimeXT <57262756+eXTrimeXT@users.noreply.github.com>
2021-04-13 16:05:42 +0300

    Update conf.py

--- conf.py
+++ conf.py
@@ -29,25 +29,7 @@ API_HASH = '9ff1a639196c0779c86dd661af8522ba'       # api hash
 # ["5", "+79606137893", "3055106", "10fb2dac961e4061dc2b8940362ae37b", "MQLRETwKE3P2ThYKMnfysgZ9G3M6VZNpoD", "extrimesystemadmin777666"],
 # Dont have a session
 
-# ["ID", "PHONE", API_ID", "API_HASH", "dogecoin", "litecoin", "password"]
+# ["ID", "PHONE", "password"]
 Accounts = (
-    ["1", "+79656203902", "extrimesystemadmin777666"],
-    ["2", "+79867663509", "extrimexakepskills777"],
-    ["3", "+79081515722", "extrimexakepskills777"],
-    ["4", "+79163833802", "extrimesystemadmin777666"],
-    ["5", "+79118238397", "extrimesystemadmin777666"],
-    ["6", "+79818739743", "extrimesystemadmin777666"],
-    ["7", "+79252564220", "extrimesystemadmin777666"],
-    ["8", "+79858243899", "extrimesystemadmin777666"],)
-
-
-
-# Accounts = (
-#     ["1", "+79656203902", 3734788, "251a28c1bfaef0e6aa38df5aaddc0f98", "extrimesystemadmin777666"],
-#     ["2", "+79867663509", 2641100, "896586daf186d281b9c1ccf0207942a8", "extrimexakepskills777"],
-#     ["3", "+79081515722", 2664260, "3a347346b7119ee61d7403f4d87df200", "extrimexakepskills777"],
-#     ["4", "+79163833802", 3632505, "00218ac8ca8cf1f51b0426adfd87816f", "extrimesystemadmin777666"],
-#     ["5", "+79118238397", 3275664, "0e5788ea4bea5b55282ca1a31f0d9ac2", "extrimesystemadmin777666"],
-#     ["6", "+79818739743", 2070934, "97ada91e9c48c9df4255811a61d3816b", "extrimesystemadmin777666"],
-#     ["7", "+79252564220", int(api_id), str(api_hash), "extrimesystemadmin777666"],
-#     ["8", "+79858243899", 3945962, "88a78640132a8ef56bc1f37431e661db", "extrimesystemadmin777666"],)
+    ["1", "+390275", "пароль1"],
+    ["2", "+63509", "пароль2"],)
