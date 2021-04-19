be724af5a1c493831e2e81e3d5a41ba6e9c3baf4
eXTrimeXT <57262756+eXTrimeXT@users.noreply.github.com>
2021-04-19 18:13:11 +0300

    Add files via upload

---
+++ registration.py
@@ -0,0 +1,17 @@
+from main import *
+
+def create_session():
+	try:
+		_account_number = int(input(Fore.YELLOW + "Введите номер аккаунта: "))
+		session = str(SESSION_NAME + str(_account_number))
+		client = TelegramClient(session, API_ID, API_HASH)
+		client.start()
+		print(Fore.GREEN + f"Сессия '{SESSION_NAME}{_account_number}' создана!" + Fore.YELLOW)
+		client.send_message("me", f"I bot number {_account_number}")
+		client.disconnect()
+		print(Fore.GREEN + "Бот отключился" + Fore.YELLOW)
+	except:
+		print(Fore.RED + "Ошибка при создании сессии...")
+		raise
+	finally:
+		print(Fore.YELLOW)
